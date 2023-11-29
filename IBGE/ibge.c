50598e5cfd04936274e495ac5967bbb7f9869faa
M4RINH0 <dmmartins.dm28@gmail.com>
2023-11-29 15:19:57 -0300

    atualização e laboratorio de ordenação

--- IBGE/ibge.c
+++ IBGE/ibge.c
@@ -3,41 +3,48 @@
 #include <string.h>
 
 #define MAX_MUNICIPIO 6000
-#define MAX_LINE_SIZE 100
+#define MAX_LINE_SIZE 256
 
-struct IBGE
+typedef struct IBGE
 {
     int IBGE;
     int linha;
-};
+} IBGE;
 
-int buscaSequencial(struct IBGE *vetor, int nlinhas, int IBGEbuscado) {
+int buscaSequencial(IBGE *vetor, int nlinhas, int IBGEbuscado) {
     for (int i = 0; i < nlinhas; ++i) {
         if (vetor[i].IBGE == IBGEbuscado) {
-            return i;
+            return vetor[i].linha;
         }
     }
     return -1;
 }
 
 int main() {
-    FILE* arquivo = fopen("/home/douglas/EDA1/IBGE/Lista_Municípios_com_IBGE_Brasil_Versao_CSV.csv", "r");
+    FILE *arquivo = fopen("Lista_Municípios_com_IBGE_Brasil_Versao_CSV.csv", "r");
     struct IBGE vetorIBGE[MAX_MUNICIPIO];
-    int numLinhas = 0;
-
+    int numLinhas = 0, ibgebuscado;
+    const int colunaIBGE = 1;
     char linha[MAX_LINE_SIZE];
-    while (fgets(linha, sizeof(linha), arquivo) != NULL && numLinhas < MAX_MUNICIPIO) {
-        // Procurar a posição de "IBGE;" na linha
-        char* posicaoIBGE = strstr(linha, "IBGE;");
-        
-        // Se "IBGE;" estiver presente na linha, extrair o valor do IBGE e a linha correspondente
-        if (posicaoIBGE != NULL) {
-            char *token = strtok(posicaoIBGE + 5, ";"); // +5 para ignorar "IBGE;"
-            vetorIBGE[numLinhas].IBGE = atoi(token);
-            vetorIBGE[numLinhas].linha = numLinhas;
-            ++numLinhas;
-        }
+
+    printf("Digite o IBGE a ser buscado: ");
+    scanf("%d", &ibgebuscado);
+    
+    // Ignorar o cabeçalho
+    fgets(linha, sizeof(linha), arquivo);
+
+    while (fgets(linha, sizeof(linha), arquivo) != NULL) {
+        char *token = strtok(linha, ";");
+        vetorIBGE[numLinhas].IBGE = atoi(token);
+        vetorIBGE[numLinhas].linha = numLinhas + 1;
+        numLinhas++;
     }
+
+    int linhaencontrada = buscaSequencial(vetorIBGE, numLinhas, ibgebuscado);
+    if (linhaencontrada != -1)
+        printf("IBGE encontrado na linha %d\n", linhaencontrada);
+    else
+        printf("IBGE não encontrado\n");
 
     fclose(arquivo);
 
